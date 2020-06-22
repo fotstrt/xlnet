d32a2c287b60bbc6f9f43608bfd4773a754b34a5
Fot_St <fotstrt95@gmail.com>
2020-06-22 04:50:51 +0300

    gg

--- run_classifier.py
+++ run_classifier.py
@@ -743,8 +743,8 @@ def main(_):
     #     model_fn=model_fn,
     #     config=run_config)
 
-    estimator = tf.estimator.Estimator(model_fn=model_fn, config=tf.estimator.RunConfig(model_dir='./my_model', save_summary_steps=FLAGS.save_checkpoint_steps,
-                save_checkpoints_steps=FLAGS.save_checkpoints_steps), params={"batch_size": FLAGS.train_batch_size})
+    estimator = tf.estimator.Estimator(model_fn=model_fn, config=tf.estimator.RunConfig(model_dir='./my_model', save_summary_steps=FLAGS.save_steps,
+                save_checkpoints_steps=FLAGS.save_steps), params={"batch_size": FLAGS.train_batch_size})
 
   if FLAGS.do_eval or FLAGS.do_predict:
     if FLAGS.eval_split == "dev":

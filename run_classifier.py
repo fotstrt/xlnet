65674ca6d36fea628cb26716377a272b6b273125
Fot_St <fotstrt95@gmail.com>
2020-06-22 22:52:55 +0300

    hh

--- run_classifier.py
+++ run_classifier.py
@@ -672,7 +672,7 @@ def serving_input_fn():
     input_ids   = tf.placeholder(tf.int64, [None, FLAGS.max_seq_length], name='input_ids')
     input_mask  = tf.placeholder(tf.float32, [None, FLAGS.max_seq_length], name='input_mask')
     segment_ids = tf.placeholder(tf.int64, [None, FLAGS.max_seq_length], name='segment_ids')
-    is_real_example = tf.placeholder(tf.int64, [None, FLAGS.max_seq_length], name='is_real_example')
+    is_real_example = tf.placeholder(tf.int64, [None], name='is_real_example')
     input_fn = tf.estimator.export.build_raw_serving_input_receiver_fn({
         'label_ids': label_ids,
         'input_ids': input_ids,
